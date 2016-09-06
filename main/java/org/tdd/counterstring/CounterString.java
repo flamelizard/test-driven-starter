ff0f49e2bb2c0eb3e8df075d54baa630f2388e97
Tom Sedlak <tom.stranger2@gmail.com>
2016-09-06 18:36:57 +0200

    My first TDD - CounterString class

---
+++ main/java/org/tdd/counterstring/CounterString.java
@@ -0,0 +1,61 @@
+package org.tdd.counterstring;
+
+/**
+ * Created by Tom on 9/3/2016.
+ */
+/*
+Developed through TDD - development guided by tests
+
+It was quite fun to pass each test starting from the simplest case of class
+requirements.
+ */
+public class CounterString {
+
+    //    Improved design based on House Of Test Generator
+    public static String generate(int length, String delimiter) {
+        StringBuilder string = new StringBuilder();
+        int interimStringLen;
+        int digit = length;
+        while (digit > 0) {
+
+            if (digit == 1) {
+                string.insert(0, delimiter);
+            } else {
+                string.insert(0, digit + delimiter);
+            }
+            interimStringLen = string.length();
+            digit = length - interimStringLen;
+        }
+        return string.toString();
+    }
+
+    public static String generate(int length) {
+        String delimiter = "*";
+        return generate(length, delimiter);
+    }
+
+    public static String generateOriginal(int length, String delimiter) {
+        StringBuilder string = new StringBuilder();
+        switch (length) {
+            case 0:
+                return "";
+            case 1:
+                return "*";
+        }
+        int interimStringLen;
+        int digit;
+        while (true) {
+            interimStringLen = string.length();
+            digit = length - interimStringLen;
+
+            if (digit > 1) {
+                string.insert(0, digit + delimiter);
+            } else {
+                if (digit == 1) string.insert(0, delimiter);
+                break;
+            }
+        }
+        return string.toString();
+    }
+
+}
---
+++ test/java/org/tdd/counterstring/CounterStringTest.java
@@ -0,0 +1,104 @@
+package org.tdd.counterstring;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Tom on 9/3/2016.
+ */
+/*
+TDD and unit tests are great tools.
+
+See immediately what works and what does not by running unit tests.
+Unit tests describe what class behaviour is - no more, no less.
+
+Make a change and run tests to see what you did break :-)
+ */
+public class CounterStringTest {
+
+    private int length;
+
+    @Test
+    public void returnsEmptyStringOnZeroLength() throws Exception {
+        length = 0;
+
+        assertThat(CounterString.generate(length), isEmptyOrNullString());
+    }
+
+    @Test
+    public void returnsDelimiterOnLengthOne() {
+        length = 1;
+
+        assertThat(CounterString.generate(length), equalTo("*"));
+    }
+
+    @Test
+    public void returnsStringForLength2() {
+        length = 2;
+
+        assertThat(CounterString.generate(length), equalTo("2*"));
+    }
+
+    @Test
+    public void returnsStringForLength3() {
+        length = 3;
+
+        assertThat(CounterString.generate(length), equalTo("*3*"));
+    }
+
+    @Test
+    public void returnsStringForLength4() {
+        length = 4;
+
+        assertThat(CounterString.generate(length), equalTo("2*4*"));
+    }
+
+    @Test
+    public void returnsStringForLength5() {
+        length = 5;
+
+        assertThat(CounterString.generate(length), equalTo("*3*5*"));
+    }
+
+    @Test
+    public void returnsStringForLength10() {
+        length = 10;
+
+        String cString = CounterString.generate(length);
+
+        assertThat(cString, equalTo("*3*5*7*10*"));
+        assertTrue(cString.length() == length);
+    }
+
+    @Test
+    public void length15() {
+        length = 15;
+        assertThat(CounterString.generate(length), equalTo("*3*5*7*9*12*15*"));
+    }
+
+    @Test
+    public void matchesLengthFor50() {
+        length = 50;
+        assertTrue(CounterString.generate(length).length() == length);
+    }
+
+    @Test
+    public void matchesLengthFor100() {
+        length = 100;
+        assertTrue(CounterString.generate(length).length() == length);
+    }
+
+//    This piece usually does not belong to tests that should be self-validating
+
+//    @Test
+//    public void visualTest() {
+//        int[] lengths = {20, 21, 50, 51, 52, 100, 150};
+//        for (int length: lengths) {
+//            System.out.println(CounterString.generate(length));
+//        }
+//    }
+}
